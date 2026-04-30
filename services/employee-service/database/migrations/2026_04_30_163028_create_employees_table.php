1014a681746196d8cf6ec87d01b3b213c54dcf80
hendrvy <hendryyyh.25@gmail.com>
2026-04-30 23:40:00 +0700

    chore: setup Laravel 11 for employee service and add database migrations for 4 related tables

---
+++ services/employee-service/database/migrations/2026_04_30_163028_create_employees_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('employees', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('user_id'); // Disinkronkan dengan ID dari Auth Service
+            $table->foreignId('position_id')->constrained('positions')->onDelete('cascade');
+            $table->string('nip')->unique();
+            $table->string('nama_lengkap');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('employees');
+    }
+};
